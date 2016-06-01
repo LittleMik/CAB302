ec5fab77b9273c944e583fc847557928cd71f9ce
Michael Smallcombe <michael.smallcombe@gmail.com>
2016-06-01 13:45:52 +1000

    Git Fix Charts

--- Assignment2/AircraftSimulator/src/asgn2Simulators/SimulationRunner.java
+++ Assignment2/AircraftSimulator/src/asgn2Simulators/SimulationRunner.java
@@ -9,17 +9,7 @@ package asgn2Simulators;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
-import java.util.Random;
 
-import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.general.Dataset;
-import org.jfree.data.time.Day;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
@@ -127,17 +117,21 @@ public class SimulationRunner {
 	 */
 	public void runSimulation(GUISimulator gs) throws AircraftException, PassengerException, SimulationException, IOException {
 		String outPutString = "";	
-		int maxQueue = 0;
 		
+		//Set Chart Numbers
+		int chart1 = 1;
+		int chart2 = 2;
 		
 		this.sim.createSchedule();
 		this.log.initialEntry(this.sim);
-
 		
-		//Chart Dataset
-		Dataset chart1Dataset = createChart1Dataset();
+		//Setup Chart Datasets
+		XYSeriesCollection chart1Dataset = createDataset(chart1);
+		XYSeriesCollection chart2Dataset = createDataset(chart2);
+		
 		String timeLog = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
 		outPutString = timeLog + ": Start of Simulation\n" + sim.toString() + "\n" + sim.getFlights(Constants.FIRST_FLIGHT).initialState();
+		
 		//Main simulation loop 
 		for (int time=0; time<=Constants.DURATION; time++) {
 			this.sim.resetStatus(time); 
@@ -145,10 +139,8 @@ public class SimulationRunner {
 			this.sim.generateAndHandleBookings(time);
 			this.sim.processNewCancellations(time);
 			
-			//Get Max Queue Size
-			if(this.sim.numInQueue() > maxQueue){
-				maxQueue = this.sim.numInQueue();
-			}
+			//Update Chart2 Dataset
+			updateDataset(chart2, chart2Dataset, null, time);
 			
 			if (time >= Constants.FIRST_FLIGHT) {
 				this.sim.processUpgrades(time);
@@ -160,9 +152,9 @@ public class SimulationRunner {
 	
 				//outPutString = outPutString + flights.getStatus(time)+"\r\n";
 				
-				//Update Chart
+				//Update Chart1 Dataset
 				Bookings b = this.sim.getFlightStatus(time);
-				updateDataset((XYSeriesCollection) chart1Dataset, b, time);
+				updateDataset(chart1, chart1Dataset, b, time);
 			} else {
 				this.sim.processQueue(time);
 			}
@@ -181,61 +173,69 @@ public class SimulationRunner {
 		outPutString = outPutString + sim.finalState();		
 		gs.addToGUI(outPutString);
 		
-		//Get Chart2 Dataset
-		Dataset chart2Dataset = createDataset(maxQueue, this.sim.numRefused());
 		//Add Charts to GUI
-		gs.addChart((XYSeriesCollection)chart1Dataset, 1);
-		//gs.addChart((XYSeriesCollection)chart2Dataset, 2);
+		gs.addChart(chart1Dataset, chart1);
+		gs.addChart(chart2Dataset, chart2);
 	}
 	
 	/**
 	 * Create an XYSeriesCollection with all series setup
+	 * according to the chartNumber specified
 	 * @return XYSeriesCollection dataset
 	 */
-	private XYSeriesCollection createChart1Dataset(){
+	private XYSeriesCollection createDataset(int chartNumber){
 		XYSeriesCollection dataset = new XYSeriesCollection();
-		XYSeries firstTotal = new XYSeries("First");
-		XYSeries businessTotal = new XYSeries("Business");
-		XYSeries premiumTotal = new XYSeries("Premium");
-		XYSeries economyTotal = new XYSeries("Economy");
-		XYSeries passengerTotal = new XYSeries("Total");
-		XYSeries seatsAvailable = new XYSeries("Seats Available");
-		
-		dataset.addSeries(firstTotal);
-		dataset.addSeries(businessTotal);
-		dataset.addSeries(premiumTotal);
-		dataset.addSeries(economyTotal);
-		dataset.addSeries(passengerTotal);
-		dataset.addSeries(seatsAvailable);
+		if(chartNumber == 1){
+			//Setup Chart1 Dataset
+			XYSeries firstTotal = new XYSeries("First");
+			XYSeries businessTotal = new XYSeries("Business");
+			XYSeries premiumTotal = new XYSeries("Premium");
+			XYSeries economyTotal = new XYSeries("Economy");
+			XYSeries passengerTotal = new XYSeries("Total");
+			XYSeries seatsAvailable = new XYSeries("Seats Available");
+			
+			dataset.addSeries(firstTotal);
+			dataset.addSeries(businessTotal);
+			dataset.addSeries(premiumTotal);
+			dataset.addSeries(economyTotal);
+			dataset.addSeries(passengerTotal);
+			dataset.addSeries(seatsAvailable);
+		}else{
+			//Setup Chart2 Dataset
+			XYSeries queueSize = new XYSeries("Queue Size");
+			XYSeries passengersRefused = new XYSeries("Passengers Refused");
+			
+			dataset.addSeries(queueSize);
+			dataset.addSeries(passengersRefused);
+		}
 		return dataset;
 	}
 	
-	private CategoryDataset createDataset(int maxQueue, int numRefused){           
-		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-		
-		dataset.addValue(maxQueue, "Passengers", "Queue Size");        
-		dataset.addValue(numRefused, "Passengers", "Passengers Refused");        
-		dataset.addValue(3 , "Passengers", "Daily Passenger Capacity");                        
-		
-		return dataset; 
-	 }
 	/**
-     * Update Dataset's series with new plot points
+     * Update a dataset's series with new plot points at the specified time
+     * according to the chartNumber specified
 	 */
-	private void updateDataset(XYSeriesCollection dataset, Bookings b, int time) {
-		XYSeries firstTotal = dataset.getSeries(0);
-		XYSeries businessTotal = dataset.getSeries(1);
-		XYSeries premiumTotal = dataset.getSeries(2);
-		XYSeries economyTotal = dataset.getSeries(3);
-		XYSeries passengerTotal = dataset.getSeries(4);
-		XYSeries seatsAvailable = dataset.getSeries(5);
-		
-	    firstTotal.add(time, b.getNumFirst());
-	    businessTotal.add(time, b.getNumBusiness());
-	    premiumTotal.add(time, b.getNumPremium());
-	    economyTotal.add(time, b.getNumEconomy());
-	    passengerTotal.add(time, b.getTotal());
-	    seatsAvailable.add(time, b.getAvailable());
+	private void updateDataset(int chartNumber, XYSeriesCollection dataset, Bookings b, int time) {
+		if(chartNumber == 1){
+			XYSeries firstTotal = dataset.getSeries(0);
+			XYSeries businessTotal = dataset.getSeries(1);
+			XYSeries premiumTotal = dataset.getSeries(2);
+			XYSeries economyTotal = dataset.getSeries(3);
+			XYSeries passengerTotal = dataset.getSeries(4);
+			XYSeries seatsAvailable = dataset.getSeries(5);
+			
+		    firstTotal.add(time, b.getNumFirst());
+		    businessTotal.add(time, b.getNumBusiness());
+		    premiumTotal.add(time, b.getNumPremium());
+		    economyTotal.add(time, b.getNumEconomy());
+		    passengerTotal.add(time, b.getTotal());
+		    seatsAvailable.add(time, b.getAvailable());
+		}else{
+			XYSeries queueSize = dataset.getSeries(0);
+			XYSeries passengersRefused = dataset.getSeries(1);
+			
+			queueSize.add(time, this.sim.numInQueue());
+			passengersRefused.add(time, this.sim.numRefused());
+		}
 	}
-	
 }
